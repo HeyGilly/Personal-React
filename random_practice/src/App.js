81e0573eefb389b4b972beffa93fdb834b3e925e
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-17 17:27:24 -0500

    Created two input that capture the value input and console log the value.

--- random_practice/src/App.js
+++ random_practice/src/App.js
@@ -1,23 +1,36 @@
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //Capture the input and stores in an
+    const [val, setVal] = useState("");
+    const [val2, setVal2] = useState("");
+
+    // Console log the value after every push.
+    // Adding a dependency array this will allow you to showcase only this console log.
+    useEffect(()=> {
+        console.log(`field 1: ${val}`);
+    }, [val])
+
+    // Console log the value after every push.
+    // Adding a dependency array this will allow you to showcase both values.
+    useEffect(()=> {
+        console.log(`field 2: ${val2}`);
+    }, [val,val2])
+
+
+    return (
+        <>
+            <label>
+                Favorite Phrase:
+                <input value={val} onChange={e=> setVal(e.target.value)}/>
+            </label>
+            <br />
+            <label>
+                Second Favorite Phrase:
+                <input value={val2} onChange={e=> setVal2(e.target.value)}/>
+            </label>
+        </>
+    )
 }
 
 export default App;
