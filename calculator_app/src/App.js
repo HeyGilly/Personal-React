a5e71ffab0c3a0b49accc8b1ffffe7203325533f
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-16 20:02:43 -0500

    Create a function that created a new array and used a for loop to create and push new buttons/numbers to the array. Then displayed the number by calling the function in the digits section

--- calculator_app/src/App.js
+++ calculator_app/src/App.js
@@ -1,9 +1,51 @@
 
 
 function App() {
+    const createDigits = () => {
+        // Create an array of digits that will store your array elements ( 1,2,3,4,5,6,7,8,9)
+        const digits = [];
+
+        // a for loop that will create the numbers 1 -9
+        // this will push the numbers that are now buttons to the digits array.
+        for(let xy = 1; xy <10;xy++ ){
+            digits.push(
+                <button key={xy}>{xy}</button>
+            )
+        }
+        return digits;
+    }
+
+
   return (
     <div className="App">
-      Hello World!
+
+        <main className={"calculator"}>
+            {/*Display of results and numbers*/}
+            <section className={"display"}>
+              <span>(0)</span> 0
+            </section>
+            {/*Buttons*/}
+            <section className={"operators"}>
+                <button>/</button>
+                <button>*</button>
+                <button>+</button>
+                <button>-</button>
+                <button>C</button>
+            </section>
+
+            {/*Going to use a function that will display the numbers for us. */
+            /*If we wrote it, the code will be long and repetitive. */
+            /*long list of buttons*/}
+            <section className={"digits"}>
+                { createDigits() }    {/*} //This is calling the function*/}
+                <button>0</button>
+                <button>.</button>
+                <button>=</button>
+            </section>
+
+        </main>
+
+
     </div>
   );
 }
