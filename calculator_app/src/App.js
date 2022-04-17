0cb4a2353baf114dc2cdb6e3f72fcb7ccd8c709a
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-16 22:06:39 -0500

    Finish the calculator. Onclick is fixed with functions. created a function that deleted, update and result that helped with the display.

--- calculator_app/src/App.js
+++ calculator_app/src/App.js
@@ -1,34 +1,74 @@
-
+import { useState } from 'react';
 
 function App() {
+    const [calc, setCalc] = useState("");
+    const [result, setResult] = useState("");
+
+    const ops = ['/','*','+','-','.'];
+
+    const updateCal = value => {
+        // if statement
+        const option1 = ops.includes(value) && calc === '';
+        const option2 = ops.includes(value) && ops.includes(calc.slice(-1))
+
+        if (option1 || option2) {return;}
+        else{ setCalc(calc + value)};
+
+        //if the last item was not a value of an ops (operator)
+        if(!ops.includes(value)){
+            setResult(eval(calc + value).toString());
+        }
+    }
+
     const createDigits = () => {
-        // Create an array of digits that will store your array elements ( 1,2,3,4,5,6,7,8,9)
+        // Create an array of digits that will store your array elements [ 1,2,3,4,5,6,7,8,9]
         const digits = [];
 
         // a for loop that will create the numbers 1 -9
         // this will push the numbers that are now buttons to the digits array.
         for(let xy = 1; xy <10;xy++ ){
             digits.push(
-                <button key={xy}>{xy}</button>
+                <button key={xy} onClick={()=> updateCal(xy.toString())}>{xy}</button>
             )
         }
         return digits;
     }
+
+    // result key
+    const calculate = () => {
+        setCalc(eval(calc).toString());
+    }
+
+    //Delete button
+    const deleteLast = () =>{
+        if (calc == " "){
+            return;
+        }
+
+        const value = calc.slice(0,-1);
+
+        setCalc(value);
+    }
+
+
+    // building of html
   return (
     <div className="App">
 
         <main className={"calculator"}>
             {/*Display of results and numbers*/}
             <section className={"display"}>
-              <span>(0)</span> 0
+                {result ? <span>({result})</span> : ''}
+                &nbsp;  {/* non breaking space*/}
+                { calc || "0"}
             </section>
             {/*---Buttons---*/}
             <section className={"operators"}>
-                <button>/</button>
-                <button>*</button>
-                <button>+</button>
-                <button>-</button>
-                <button>C</button>
+                <button onClick={()=> updateCal('/')}>/</button>
+                <button onClick={()=> updateCal('*')}>*</button>
+                <button onClick={()=> updateCal('+')}>+</button>
+                <button onClick={()=> updateCal('-')}>-</button>
+                <button onClick={deleteLast}>C</button>
             </section>
 
             {/*Going to use a function that will display the numbers for us. */
@@ -36,9 +76,9 @@ function App() {
             /*long list of buttons*/}
             <section className={"digits"}>
                 { createDigits() }    {/*} //This is calling the function*/}
-                <button>0</button>
-                <button>.</button>
-                <button>=</button>
+                <button onClick={()=> updateCal('0')}>0</button>
+                <button onClick={()=> updateCal('.')}>.</button>
+                <button onClick={calculate}>=</button>
             </section>
 
         </main>
