48299b282ba53c04bf78f9fe789bfd8d57783047
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-19 21:01:46 -0500

    feature: Created a search bar with a placeholder and classnames

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,7 +1,24 @@
 
+const Weather_API = {
+    keys: "c6734099ed5daa0f854247202a5a8f59",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
 
 function App() {
-
+    return (
+     <div className={"app"}>
+         <main>
+             <section className={"search-container"}>
+                 <input
+                     type={"Text"}
+                     className={"searchBar"}
+                     placeholder={"Search Here..."}
+                 />
+             </section>
+         </main>
+     </div>
+    )
 }
 
 export default App;
