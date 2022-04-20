ab43e8d0ac86411d0e0d62ab33f7b6921d7e1cb6
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-20 01:14:46 -0500

    feature: inputed the function that will fetch the data and then display it in the correct location. In the input you search for any city and then push enter, in console log you will see all the data for the city searched.

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,16 +1,32 @@
+import pic from "./img/icons/sunny.png";
+import {useState} from "react";
 import Moment from 'moment';
-
-
 
 const Weather_API = {
     keys: "c6734099ed5daa0f854247202a5a8f59",
     base: "https://api.openweathermap.org/data/2.5/"
 }
 
-
 function App() {
+    // This is for the date
     const formatDate = Moment().format("MMM Do YY");
 
+    //UseState will change the data
+    const [locationData, setData] = useState('');
+    const [weather, setWeather] = useState({});
+
+    // search function
+    const search = e => {
+        if (e.key === 'Enter'){
+            fetch(`${Weather_API.base}weather?q=${locationData}&units=metric&APPID=${Weather_API.keys}`)
+            .then(res  => res.json())      //turns into json and readable
+                .then(result => {
+                    setWeather(result)
+                    setData('');
+                    console.log(result)
+                })
+        }
+    }
 
 
 
@@ -22,16 +38,33 @@ function App() {
                      type={"Text"}
                      className={"searchBar"}
                      placeholder={"City..."}
+                     onChange={e => setData(e.target.value)}
+                     value={locationData}
+                     onKeyPress={search}  // when enter button is pushed it will run function
                  />
              </section>
-             <section className={"location-container"}>
-                 <p className={"location"}>Los Angeles, California</p>
-                 <p className={"date"}>{formatDate}</p>
-             </section>
-             <section className={"weather-container"}>
-                 <p className={"temp"}> 72˚F</p>
-                 <p className={"weather"}>Cloudy</p>
-             </section>
+
+             {/*If statement that when on start up you will not get an error, instead you will get a welcome page.*/}
+             {(typeof weather.main != 'undefined') ? (
+                 <div>
+                     <section className={"location-container"}>
+                         <p className={"location"}>{weather.name},{weather.sys.country}</p>
+                         <p className={"date"}>{formatDate}</p>
+                     </section>
+                     <section className={"weather-container"}>
+                         <p className={"temp"}> 72˚F</p>
+                         <p className={"weather"}>Sunny</p>
+                         <div className={'img-container'}>
+                             <img src={pic}  alt={'sunny'}/>
+                         </div>
+                     </section>
+                 </div>
+             ) : (
+                 <div className={"welcomePage"}>
+                     <h1>Welcome to Weather App</h1>
+                     <small>Search for any city and press Enter</small>
+                 </div>
+             )}
          </main>
      </div>
     )
