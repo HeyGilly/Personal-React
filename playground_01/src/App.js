800c6f29f60e30f5d67f4a64984c3712107b15a0
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-04 20:33:01 -0500

    multiply components

--- playground_01/src/App.js
+++ playground_01/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 
+function SetOfPersonalSkills({setSkills}){
+    return <p>{setSkills}</p>;
+}
+
+
+
 // App Component
 function Playground_Component({username, currentTime}) {
   return (
@@ -7,6 +13,13 @@ function Playground_Component({username, currentTime}) {
         <h1>Welcome to the playground, {username}!</h1>
         <div className="timeContainer">
             <p>{currentTime}</p>
+        </div>
+        <div className="skillsContainer">
+            <SetOfPersonalSkills setSkills="Javascript" />
+            <SetOfPersonalSkills setSkills="React" />
+            <SetOfPersonalSkills setSkills="Java" />
+            <SetOfPersonalSkills setSkills="Spring" />
+            <SetOfPersonalSkills setSkills="Python" />
         </div>
     </>
   );
