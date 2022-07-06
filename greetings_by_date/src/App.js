a6060d8e9f3d826074418d253ca18c6868b21957
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-06 13:39:08 -0500

    Create the images that will be used and also go the time to display in  24hour time.

--- greetings_by_date/src/App.js
+++ greetings_by_date/src/App.js
@@ -1,21 +1,49 @@
 import './App.css';
 
-function App(displayCurrentTime) {
-  return (
-    <>
-        <nav className="navbar bg-light">
-            <div className="container-fluid">
-                <h1 className={"text-center w-100 text-dark display-4 pt-4 pb-2"}>Welcome to the playground!</h1>
-            </div>
-        </nav>
-        <main className={"vh-100 bg-dark"}>
+function Morning({props}){
+    return(
+        <>
+            <img src={"img/goodMorning.png"} alt={"Good Morning World!"} className={"mx-auto"} height={'auto'} width={"820px"}/>
+        </>
+    );
+}
+function Evening({props}){
+    return(
+        <>
+            <img src={"img/goodNight.png"} alt={"Good Evening World!"} className={"mx-auto"} height={'auto'} width={"820px"}/>
+        </>
+    );
+}
+function LateNight({props}){
+    return(
+        <>
+            <img src={"img/midnight.png"} alt={"Late Night!"} className={"mx-auto"} height={'auto'} width={"820px"}/>
+        </>
+    );
+}
 
 
+function App({currentDate, currentHour}) {
+    return (
+        <>
+            <nav className="navbar bg-light">
+                <div className="container-fluid">
+                    <h1 className={"text-center w-100 text-dark display-4 pt-4 pb-2"}>Welcome to the playground!</h1>
+                </div>
+            </nav>
+            <main className={"vh-100 bg-dark pt-5"}>
+                <p className={"text-center text-dark rounded-5 w-25 mx-auto p-2 bg-warning"}>({currentDate})</p>
+                <p>{currentHour}</p>
+                <section className={"d-flex flex-column justify-content-center "}>
+                    <Morning />
+                    <Evening />
+                    <LateNight />
+                </section>
 
-
-        </main>
-    </>
-  );
+            </main>
+        </>
+    );
 }
 
 export default App;
+
--- greetings_by_date/src/index.js
+++ greetings_by_date/src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 
 
-
-
-
+// The Current time
+const current = new Date();
+const CurrentDate = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+const CurrentHourRightNow = `${current.getHours()}`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App currentDate={CurrentDate} currentHour={CurrentHourRightNow}/>
   </React.StrictMode>
 );
