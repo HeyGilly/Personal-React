c89c6e1f81a5b420f4a25d32c17231b645146210
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-06 12:31:30 -0500

    Initial Commit

---
+++ greetings_by_date/README.md
@@ -0,0 +1,2 @@
+#Greetings From The Time
+This project is going to display a specific greeting that is going to be based on the time of day. 
---
+++ greetings_by_date/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App(displayCurrentTime) {
+  return (
+    <>
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+                <h1 className={"text-center w-100 text-dark display-4 pt-4 pb-2"}>Welcome to the playground!</h1>
+            </div>
+        </nav>
+        <main className={"vh-100 bg-dark"}>
+
+
+
+
+        </main>
+    </>
+  );
+}
+
+export default App;
