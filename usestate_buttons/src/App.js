6467515ce35cff5db8be28643a23b74f064ad67b
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-07 23:03:01 -0500

    Using the useState hook to change the state in the application

--- usestate_buttons/src/App.js
+++ usestate_buttons/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [feelingStatus, setFeelingStatus] = useState("Default");
   return (
     <>
+      <h1>Welcome!</h1>
+      <h3>Today we are feeling: {feelingStatus}</h3>
 
+      <button onClick={()=> setFeelingStatus("Happy")}>Happy</button>
+      <button onClick={()=> setFeelingStatus("Tired")}>Tired</button>
+      <button onClick={()=> setFeelingStatus("Excited")}>Excited</button>
     </>
   );
 }
