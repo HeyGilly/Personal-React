1f0e909edb1930bd708cbe6af11a7d2a44f4b376
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-07 23:37:35 -0500

    Using the useState hook to change the state in the application

--- usestate_buttons/src/App.js
+++ usestate_buttons/src/App.js
@@ -2,15 +2,26 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [feelingStatus, setFeelingStatus] = useState("Default");
+    const [usernameStatus, setUsernameStatus] = useState("LostMindedDev");
+    const [feelingStatus, setFeelingStatus] = useState("Crazy");
   return (
     <>
-      <h1>Welcome!</h1>
-      <h3>Today we are feeling: {feelingStatus}</h3>
-
-      <button onClick={()=> setFeelingStatus("Happy")}>Happy</button>
-      <button onClick={()=> setFeelingStatus("Tired")}>Tired</button>
-      <button onClick={()=> setFeelingStatus("Excited")}>Excited</button>
+        <header className={"text-center pt-5 pb-5 bg-warning"}>
+            <h1>Welcome <span className={"text-decoration-underline"}>{usernameStatus}</span>! </h1>
+            <h2>Today we are feeling: <span className={"text-decoration-underline"}>{feelingStatus}</span></h2>
+        </header>
+        <div className={"d-flex row justify-content-center m-5"}>
+            <h4 className={"text-center"}>Change how you feeling today?</h4>
+            <button type={"button"} className={"btn btn-dark col-3"} onClick={()=> setFeelingStatus("Happy")}>Happy</button>
+            <button type={"button"} className={"btn btn-dark col-3 mx-5"} onClick={()=> setFeelingStatus("Tired")}>Tired</button>
+            <button type={"button"} className={"btn btn-dark col-3"} onClick={()=> setFeelingStatus("Excited")}>Excited</button><br />
+        </div>
+        <div className={"d-flex row justify-content-center m-5"}>
+            <h4 className={"text-center"}>Change usernames</h4>
+            <button type={"button"} className={"btn btn-dark col-3"} onClick={()=> setUsernameStatus("HappyGilmore")}>HappyGilmore</button>
+            <button type={"button"} className={"btn btn-dark col-3 mx-5"} onClick={()=> setUsernameStatus("TiredSusy")}>TiredSusy</button>
+            <button type={"button"} className={"btn btn-dark col-3"} onClick={()=> setUsernameStatus("LonelyTraveler")}>LonelyTraveler</button>
+        </div>
     </>
   );
 }
