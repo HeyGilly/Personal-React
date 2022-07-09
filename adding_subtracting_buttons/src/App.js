f0decf63f7ea34cfd42f794c7081436bfea73887
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-08 23:50:31 -0500

    Got two buttons to display one for adding one for subtracting

--- adding_subtracting_buttons/src/App.js
+++ adding_subtracting_buttons/src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div>
+          <section>
+              <button type="button" className="btn btn-success">Success</button>
+          </section>
+          <section>
 
-    </div>
+          </section>
+          <section>
+              <button type="button" className="btn btn-danger">Danger</button>
+          </section>
+      </div>
+
+
+
+    </>
   );
 }
 
