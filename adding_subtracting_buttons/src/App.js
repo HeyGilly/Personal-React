ef3883e39dc3dcf42f033f8fbb25c8851bb5c3a8
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-08 23:59:37 -0500

    added a usestate to change the output of the  points. when a button is clicked on they will be changed.

--- adding_subtracting_buttons/src/App.js
+++ adding_subtracting_buttons/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import React, { useState} from "react";
 
 function App() {
+
+    const [count, setCount] = useState(0)
+
   return (
     <>
-      <div>
+      <div className={'bg-dark vh-100'}>
           <section>
-              <button type="button" className="btn btn-success">Success</button>
+              <button type="button" className="btn btn-success" onClick={() => setCount(prevCount => prevCount + 1)}>
+                  Success</button>
+          </section>
+          <section className={"text-white"}>
+              <p>{count} points!</p>
           </section>
           <section>
-
-          </section>
-          <section>
-              <button type="button" className="btn btn-danger">Danger</button>
+              <button type="button" className="btn btn-danger" onClick={() => setCount(prevCount => prevCount - 1)}>Danger</button>
           </section>
       </div>
 
