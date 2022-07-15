00b5205528ebcaebaa95b7f0e045318e3b9fb184
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-14 21:03:01 -0500

    footer added

--- playground_02/src/App.js
+++ playground_02/src/App.js
@@ -8,17 +8,31 @@ function Header(){
         <header className="bg-dark shadow p-0 m-0 w-100">
           <section className={"container py-3 d-flex align-items-center "}>
             <img src={"/favicon.ico"} className={"App-logo"} alt="logo" />
-              <p className={"text-warning pt-4 ps-3"}><b>HeyGilly PlayGround 02</b></p>
+              <p className={"text-warning pt-4 ps-3"}><b>PlayGround 02</b></p>
           </section>
         </header>
       </>
   )
 }
 
+
+function Footer({year}){
+    return(
+        <>
+            <footer className={"text-center bg-dark text-light fixed-bottom"} >
+                <p><small>{year}
+                    <a className={"text-white text-decoration-none"} href="https://www.linkedin.com/in/heygilly/"> HeyGilly</a>
+                </small></p>
+            </footer>
+        </>
+    )
+}
+
 function App() {
   return (
     <>
       <Header />
+        <Footer year={new Date().getFullYear()} />
     </>
   );
 }
