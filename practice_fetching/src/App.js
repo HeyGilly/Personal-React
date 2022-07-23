78625ca0335070bd01802e4d65db3e91f90a66a0
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-07-22 23:53:58 -0500

    Create a input with a button that once pushed it fetches the data.

--- practice_fetching/src/App.js
+++ practice_fetching/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState} from "react";
 
 //--- Header
 function Navigation(){
@@ -15,11 +16,51 @@ function Navigation(){
   )
 }
 
-
 function App() {
+    const [info, setInfo] = useState("");
+    const [userInput, setUserInput] = useState("");
+
+
+    //-- Fetching Data When Button is Called
+    const FetchThatData = () => {
+        fetch(`https://bobsburgers-api.herokuapp.com/characters/${userInput}`)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setInfo(data)
+            })
+    }
+
   return (
   <>
     <Navigation />
+      <main className={"mt-5 pt-5 mx-5 px-5"}>
+          <section className={"mt-5 pt-5 mx-5 px-5"}>
+              <div className="input-group mb-3 mt-5 pt-5 mx-5 px-5">
+                  <input type={"text"}
+                         className="form-control"
+                         placeholder="Enter Number"
+                         aria-label="Number Is Entered"
+                         aria-describedby="basic-addon2"
+                         onChange={(e)=> setUserInput(e.target.value)}
+                         value={userInput}
+                  />
+                  <div className="input-group-append">
+                          <button className="btn btn-outline-secondary" type="button" onClick={FetchThatData}>Button</button>
+                  </div>
+              </div>
+          </section>
+          <section className={"border py-5"}>
+              {info
+                  ? <pre>{JSON.stringify(info, null,2)}</pre>
+                  : <h2> No Data</h2>
+              }
+          </section>
+          <section>
+              <p>{info.name}</p>
+          </section>
+      </main>
   </>
   );
 }
